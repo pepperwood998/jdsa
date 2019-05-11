362feefc4a1777e2e75bea07171ffb63141dd352
zuratama <tuandt66742@gmail.com>
2019-05-11 23:36:28 +0700

    Add Doubly Linked List, with example

--- sample/src/main/java/com/moron/project/jdsa/Sample_1.java
+++ sample/src/main/java/com/moron/project/jdsa/Sample_1.java
@@ -1,5 +1,6 @@
 package com.moron.project.jdsa;
 
+import com.moron.project.jdsa.dast.DoublyLinkedList;
 import com.moron.project.jdsa.dast.SinglyLinkedList;
 
 /**
@@ -10,10 +11,21 @@ public class Sample_1
 {
     public static void main(String[] args)
     {
-	SinglyLinkedList<Integer> sll = new SinglyLinkedList<>();
-	sll.addToHead(3);
-	sll.addToHead(1);
-	sll.addToHead(9);
-	System.out.println(sll);
+//	SinglyLinkedList<Integer> sll = new SinglyLinkedList<>();
+//	sll.addToHead(3);
+//	sll.addToHead(1);
+//	sll.addToHead(9);
+//	System.out.println(sll);
+	
+	DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
+	dll.deleteNode(5);
+	dll.addToHead(3);
+	dll.addToHead(1);
+	dll.addToTail(100);
+	dll.addToHead(5);
+	System.out.println(dll.deleteFromTail());
+	dll.addToTail(69);
+	dll.addToTail(13);
+	System.out.println(dll);
     }
 }

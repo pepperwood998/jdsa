719a693b7327df7d615c6ead8d06f424c9d11a98
zuratama <tuandt66742@gmail.com>
2019-07-01 10:59:50 +0700

    Re-format and refractor code

--- jdsa-lib/src/main/java/com/moron/project/jdsa/dast/DoublyLinkedList.java
+++ jdsa-lib/src/main/java/com/moron/project/jdsa/dast/DoublyLinkedList.java
@@ -1,150 +1,123 @@
 package com.moron.project.jdsa.dast;
 
-public class DoublyLinkedList<T>
-{
-    private Node mHead;
-    private Node mTail;
+public class DoublyLinkedList<T> {
 
-    public DoublyLinkedList()
-    {
-	mHead = null;
-	mTail = null;
-    }
+	private Node mHead;
+	private Node mTail;
 
-    public void addToHead(final T data)
-    {
-	mHead = new Node(data, mHead, null);
-
-	if (mTail == null)
-	{
-	    mTail = mHead;
-	} else
-	{
-	    mHead.mNext.mPrev = mHead;
-	}
-    }
-
-    public void addToTail(final T data)
-    {
-	mTail = new Node(data, null, mTail);
-
-	if (mHead == null)
-	{
-	    mHead = mTail;
-	} else
-	{
-	    mTail.mPrev.mNext = mTail;
-	}
-    }
-
-    public T deleteFromHead()
-    {
-	if (this.isEmpty())
-	    return null;
-
-	T data = mHead.mData;
-
-	if (mHead == mTail)
-	{
-	    mHead = null;
-	    mTail = null;
-	} else
-	{
-	    mHead = mHead.mNext;
-	    mHead.mPrev = null;
+	public DoublyLinkedList() {
+		mHead = null;
+		mTail = null;
 	}
 
-	return data;
-    }
+	public void addToHead(final T data) {
+		mHead = new Node(data, mHead, null);
 
-    public T deleteFromTail()
-    {
-	if (this.isEmpty())
-	    return null;
-
-	T data = mTail.mData;
-
-	if (mHead == mTail)
-	{
-	    mHead = null;
-	    mTail = null;
-	} else
-	{
-	    mTail = mTail.mPrev;
-	    mTail.mNext = null;
+		if (mTail == null) {
+			mTail = mHead;
+		} else {
+			mHead.mNext.mPrev = mHead;
+		}
 	}
 
-	return data;
-    }
+	public void addToTail(final T data) {
+		mTail = new Node(data, null, mTail);
 
-    public void deleteNode(final T data)
-    {
-	if (this.isEmpty())
-	    return;
-	
-	if (data.equals(mHead.mData))
-	{
-	    mHead = mHead.mNext;
-	    mHead.mPrev = null;
-	} else
-	{
-	    Node tmp;
-	    for (tmp = mHead; tmp != null && !data.equals(tmp.mData); tmp = tmp.mNext)
-		;
-
-	    if (tmp != null)
-	    {
-		tmp.mPrev.mNext = tmp.mNext;
-
-		if (tmp == mTail)
-		    mTail = tmp.mPrev;
-	    }
-	}
-    }
-
-    public boolean isEmpty()
-    {
-	return mHead == null;
-    }
-
-    public boolean contain(final T data)
-    {
-	Node tmp;
-	for (tmp = mHead; tmp != null && !data.equals(tmp.mData); tmp = tmp.mNext)
-	    ;
-
-	return tmp == null;
-    }
-
-    @Override
-    public String toString()
-    {
-	StringBuilder res = new StringBuilder();
-
-	for (Node tmp = mHead; tmp != null; tmp = tmp.mNext)
-	{
-	    res.append(tmp.mData + " ");
+		if (mHead == null) {
+			mHead = mTail;
+		} else {
+			mTail.mPrev.mNext = mTail;
+		}
 	}
 
-	return res.toString();
-    }
+	public T deleteFromHead() {
+		if (this.isEmpty())
+			return null;
 
-    private class Node
-    {
-	T mData;
-	Node mNext;
-	Node mPrev;
+		T data = mHead.mData;
 
-	public Node(T data)
-	{
-	    this(data, null, null);
+		if (mHead == mTail) {
+			mHead = null;
+			mTail = null;
+		} else {
+			mHead = mHead.mNext;
+			mHead.mPrev = null;
+		}
+
+		return data;
 	}
 
-	public Node(T data, Node next, Node prev)
-	{
-	    mData = data;
-	    mNext = next;
-	    mPrev = prev;
+	public T deleteFromTail() {
+		if (this.isEmpty())
+			return null;
+
+		T data = mTail.mData;
+
+		if (mHead == mTail) {
+			mHead = null;
+			mTail = null;
+		} else {
+			mTail = mTail.mPrev;
+			mTail.mNext = null;
+		}
+
+		return data;
 	}
-    }
+
+	public void deleteNode(final T data) {
+		if (this.isEmpty())
+			return;
+
+		if (data.equals(mHead.mData)) {
+			mHead = mHead.mNext;
+			mHead.mPrev = null;
+		} else {
+			Node tmp;
+			for (tmp = mHead; tmp != null && !data.equals(tmp.mData); tmp = tmp.mNext)
+				;
+
+			if (tmp != null) {
+				tmp.mPrev.mNext = tmp.mNext;
+
+				if (tmp == mTail)
+					mTail = tmp.mPrev;
+			}
+		}
+	}
+
+	public boolean isEmpty() {
+		return mHead == null;
+	}
+
+	public boolean contain(final T data) {
+		Node tmp;
+		for (tmp = mHead; tmp != null && !data.equals(tmp.mData); tmp = tmp.mNext)
+			;
+
+		return tmp == null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder res = new StringBuilder();
+
+		for (Node tmp = mHead; tmp != null; tmp = tmp.mNext) {
+			res.append(tmp.mData + " ");
+		}
+
+		return res.toString();
+	}
+
+	private class Node {
+
+		T mData;
+		Node mNext;
+		Node mPrev;
+
+		public Node(T data, Node next, Node prev) {
+			mData = data;
+			mNext = next;
+			mPrev = prev;
+		}
+	}
 }
